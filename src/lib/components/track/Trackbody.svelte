9a87955fd4f72bf35120df6ddae94f550359b4da
Naoki Miyagawa <ntsome21@gmail.com>
2026-05-15 00:25:26 +0100

    comment track body, update version of adapter-auto

--- src/lib/components/track/Trackbody.svelte
+++ src/lib/components/track/Trackbody.svelte
@@ -1,101 +1,140 @@
+<!--************************************* -->
+<!-- TRACK BODY -->
+<!-- displays the bars and any audio regions in it for ONE track -->
+<!-- ************************************* -->
+
 <script lang="ts">
-// @ts-nocheck
-import { onMount } from "svelte";
-import { noOfBars, TracksArray, midiEditorHidden, whichMidiTrackOpen } from "$lib/stores";
-import AudioRegion from "./AudioRegion.svelte";
-  import NoteRegion from "../editor/NoteRegion.svelte";
+    // @ts-nocheck
+    import { onMount } from "svelte";
+    import { noOfBars, TracksArray, midiEditorHidden, whichMidiTrackOpen } from "$lib/stores";
+    import AudioRegion from "./AudioRegion.svelte";
+    import NoteRegion from "../editor/NoteRegion.svelte";
+    export let track;    // current track object
+    export let trackIndex;     // index of current track
 
-export let track;        // current track object
-export let trackIndex;   // index of this track
+    // initialise open menu x and y coordinates
+    let menuX = 0;
+    let menuY = 0;
 
-let tracksArea: HTMLElement
+    //represents the entire track body area (used to calculate where user right clicked)
+    let tracksArea: HTMLElement
 
-let showMenu = false;
-let menuX = 0;
-let menuY = 0;
+    //right click pop up menu is initially hidden 
+    let showMenu = false;
 
-// which bar and which track the user opened the menu in
-let clickedTrackIndex = null;
-let clickedBarIndex = null;
-
-// right-click handler
-function handleRightClick(e, trackIndex, barIndex) {
-
-  const rect = tracksArea.getBoundingClientRect()
-  e.preventDefault();
-  showMenu = true;
-  menuX = e.pageX - rect.left;
-  menuY = e.pageY - rect.top;
-  clickedTrackIndex = trackIndex;
-  clickedBarIndex = barIndex;
-}
-
-// close menu when clicking elsewhere
-onMount(() => {
-  const closeMenu = () => (showMenu = false);
-  document.addEventListener("click", closeMenu);
-  return () => document.removeEventListener("click", closeMenu);
-});
-
-function addRegion() {
-  const regionHere = track.regions.find(region => region.barNo === clickedBarIndex);
-
-  if (regionHere) {
-    return;  //cancel if region already exists
-  }
-
-  TracksArray.update(tracks => {
-    const copy = [...tracks];
-    const track = copy[clickedTrackIndex];
-
-    const newRegion = {
-      barNo: clickedBarIndex,
-      length: 1,
-      notes: []
-    };
-
-    track.regions = [...track.regions, newRegion];
-    copy[clickedTrackIndex] = track;
-    return copy;
-  });
-}
-
-function deleteRegion() {
-  TracksArray.update(tracks => {
-    const copy = [...tracks];
-    const track = copy[clickedTrackIndex];
-
-    track.regions = track.regions.filter(
-      region => region.barNo !== clickedBarIndex
-    );
-
-    copy[clickedTrackIndex] = track;
-    return copy;
-  });
-}
+    // which bar and which track the user opened the menu in
+    let clickedTrackIndex = null;
+    let clickedBarIndex = null;
 
 
-function toggleMidiEditor() {
-    midiEditorHidden.update(value => !value)
-    whichMidiTrackOpen.set(clickedTrackIndex)
-}
+
+    //OPEN RIGHT CLICK MENU FOR THE SELECTED TRACK AND BAR EXACTLY WHERE THE CLICK HAPPENED
+    function handleRightClick(e, trackIndex, barIndex) {
+      
+        //get the tracksArea as its dimensions
+        const tracksAreaRect = tracksArea.getBoundingClientRect()
+
+        //stop browser right click menu interfering with our custom right click menu
+        e.preventDefault();
+
+        // the right click popup menu is now showing
+        showMenu = true;
+
+        // Convert global mouse position into coordinates relative to the track container
+        menuX = e.pageX - tracksAreaRect.left;
+        menuY = e.pageY - tracksAreaRect.top;
+
+        // Store which track + bar this action applies to
+        clickedTrackIndex = trackIndex;
+        clickedBarIndex = barIndex;
+    }
+
+
+
+    //listener to close right click menu if anywhere in the track body clicked 
+    onMount(() => {
+      const closeMenu = () => (showMenu = false);
+      document.addEventListener("click", closeMenu);
+      return () => document.removeEventListener("click", closeMenu);
+    });
+
+
+
+    //ADD A NEW AUDIO REGION
+    function addRegion() {
+      const regionHere = track.regions.find(region => region.barNo === clickedBarIndex);
+
+      if (regionHere) {
+        return;  //cancel if region already exists
+      }
+
+      TracksArray.update(tracks => {
+        const copy = [...tracks];
+        const track = copy[clickedTrackIndex];
+
+        const newRegion = {
+          barNo: clickedBarIndex,
+          length: 1,
+          notes: []
+        };
+
+        track.regions = [...track.regions, newRegion];
+        copy[clickedTrackIndex] = track;
+        return copy;
+      });
+    }
+
+
+    //DELETE AN EXISTING AUDIO REGION
+    function deleteRegion() {
+      TracksArray.update(tracks => {
+        const copy = [...tracks];
+        const track = copy[clickedTrackIndex];
+
+        track.regions = track.regions.filter(
+          region => region.barNo !== clickedBarIndex
+        );
+
+        copy[clickedTrackIndex] = track;
+        return copy;
+      });
+    }
+
+
+    //OPEN MIDI EDITOR FOR THE SELECTED TRACK 
+    function toggleMidiEditor() {
+        midiEditorHidden.update(value => !value)
+        whichMidiTrackOpen.set(clickedTrackIndex)
+    }
 </script>
 
-<!-- The track’s body -->
-<!-- the double nested each is because it goes through each bar of each track to find the only bar that needs to contain a region -->
+
+<!-- LOOPS THROUGH EACH BAR OF EACH TRACK AND DISPLAYS REGION IF REGION HAS BEEN ADDED-->
 <div class="bg-neutral-900 h-15 border-neutral-600 border-r-1 flex" bind:this={tracksArea}>
 
-  {#each Array.from({ length: $noOfBars }) as _, barIndex}
-    <!-- svelte-ignore a11y_no_static_element_interactions -->
-    <div class="border-neutral-800 border-1 w-1/4 h-15 p-0 flex-shrink-0" on:contextmenu={(e) => handleRightClick(e, trackIndex, barIndex)}>
-      {#each track.regions.filter(region => region.barNo === barIndex) as region} 
-          <AudioRegion {region} {track} />
-      {/each}
-    </div>
-  {/each}
+  
+    <!-- LOOP THROUGH EACH BAR -->
+    {#each Array.from({ length: $noOfBars }) as _, barIndex}
+
+
+        <!-- THE DIV CONTAINING A BAR CELL -->
+        <div class="border-neutral-800 border-1 w-1/4 h-15 p-0 flex-shrink-0" on:contextmenu={(e) => handleRightClick(e, trackIndex, barIndex)}>
+
+
+            <!-- DISPLAY REGION IF THE BAR CONTAINS A REGION-->
+            {#each track.regions.filter(region => region.barNo === barIndex) as region} 
+                <AudioRegion {region} {track} />
+            {/each}
+
+        </div>
+
+    {/each}
+
 </div>
 
-<!-- Menu that shows when right clicked -->
+
+
+<!-- RIGHT CLICK MENU -->
 {#if showMenu}
   <ul
     class="absolute bg-white text-black rounded shadow-md"
